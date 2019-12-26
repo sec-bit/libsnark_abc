567f98da96a533844ba312e4d4e1dc382321c5b4
p0n1 <zp@zerok.io>
2019-12-26 18:35:24 +0800

    init with examples

---
+++ README.md
@@ -0,0 +1,8 @@
+# libsnark ABC
+
+Minimal example to use libsnark.
+
+## Reference
+
+- https://github.com/christianlundkvist/libsnark-tutorial
+- https://github.com/howardwu/libsnark-tutorial
---
+++ src/range.cpp
@@ -0,0 +1,51 @@
+#include <libff/common/default_types/ec_pp.hpp>
+#include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
+#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
+#include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
+#include <libsnark/gadgetlib1/pb_variable.hpp>
+#include <libsnark/gadgetlib1/gadgets/basic_gadgets.hpp>
+
+using namespace libsnark;
+using namespace std;
+
+int main () {
+    typedef libff::Fr<default_r1cs_gg_ppzksnark_pp> FieldT;
+
+    // Initialize the curve parameters
+    default_r1cs_gg_ppzksnark_pp::init_public_params();
+  
+    // Create protoboard
+    protoboard<FieldT> pb;
+
+    pb_variable<FieldT> x, max;
+    pb_variable<FieldT> less, less_or_eq;
+
+    x.allocate(pb, "x");
+    max.allocate(pb, "max");
+    
+    pb.val(max)= FieldT(60);
+
+    comparison_gadget<FieldT> cmp(pb, 10, x, max, less, less_or_eq, "cmp");
+    cmp.generate_r1cs_constraints();
+    pb.add_r1cs_constraint(r1cs_constraint<FieldT>(less, 1, FieldT::one()));
+
+    const r1cs_constraint_system<FieldT> constraint_system = pb.get_constraint_system();
+
+    // generate keypair
+    const r1cs_gg_ppzksnark_keypair<default_r1cs_gg_ppzksnark_pp> keypair = r1cs_gg_ppzksnark_generator<default_r1cs_gg_ppzksnark_pp>(constraint_system);
+
+    // Add witness values
+    pb.val(x) = FieldT(2); // secret
+    cmp.generate_r1cs_witness();
+
+    // generate proof
+    const r1cs_gg_ppzksnark_proof<default_r1cs_gg_ppzksnark_pp> proof = r1cs_gg_ppzksnark_prover<default_r1cs_gg_ppzksnark_pp>(keypair.pk, pb.primary_input(), pb.auxiliary_input());
+
+    // verify
+    bool verified = r1cs_gg_ppzksnark_verifier_strong_IC<default_r1cs_gg_ppzksnark_pp>(keypair.vk, pb.primary_input(), proof);
+
+    cout << "Number of R1CS constraints: " << constraint_system.num_constraints() << endl;
+    cout << "Primary (public) input: " << pb.primary_input() << endl;
+    cout << "Auxiliary (private) input: " << pb.auxiliary_input() << endl;
+    cout << "Verification status: " << verified << endl;
+}
