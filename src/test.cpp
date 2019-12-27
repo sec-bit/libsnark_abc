1534b9fd91599e076b5f8fb0c43b15ff8a6ac127
p0n1 <zp@zerok.io>
2019-12-27 10:29:03 +0800

    remove unused headers to keep minimal

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,4 @@
-#include <libff/common/default_types/ec_pp.hpp>
 #include <libsnark/common/default_types/r1cs_gg_ppzksnark_pp.hpp>
-#include <libsnark/relations/constraint_satisfaction_problems/r1cs/examples/r1cs_examples.hpp>
 #include <libsnark/zk_proof_systems/ppzksnark/r1cs_gg_ppzksnark/r1cs_gg_ppzksnark.hpp>
 #include <libsnark/gadgetlib1/pb_variable.hpp>
 
